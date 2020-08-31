56a9d5544f9550a512e820ad2da42c574db8ad01
aharrar <46299409+aharrar@users.noreply.github.com>
2020-08-31 11:26:54 +0300

    dll the host side

---
+++ WysHost/WysHost/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WysHost
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_wysView = new System.Windows.Forms.Panel();
+            this.button_submit = new System.Windows.Forms.Button();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_wysView
+            // 
+            this.panel_wysView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_wysView.Location = new System.Drawing.Point(46, 73);
+            this.panel_wysView.Name = "panel_wysView";
+            this.panel_wysView.Size = new System.Drawing.Size(260, 260);
+            this.panel_wysView.TabIndex = 1;
+            this.panel_wysView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_wysView_MouseDown);
+            this.panel_wysView.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel_wysView_MouseUp);
+            // 
+            // button_submit
+            // 
+            this.button_submit.BackColor = System.Drawing.Color.Gainsboro;
+            this.button_submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button_submit.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.button_submit.Location = new System.Drawing.Point(15, 460);
+            this.button_submit.Name = "button_submit";
+            this.button_submit.Size = new System.Drawing.Size(140, 32);
+            this.button_submit.TabIndex = 7;
+            this.button_submit.Text = "Submit";
+            this.button_submit.UseVisualStyleBackColor = false;
+            this.button_submit.Click += new System.EventHandler(this.button_submit_Click);
+            // 
+            // button_clear
+            // 
+            this.button_clear.BackColor = System.Drawing.Color.Gainsboro;
+            this.button_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button_clear.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.button_clear.Location = new System.Drawing.Point(188, 460);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(143, 32);
+            this.button_clear.TabIndex = 8;
+            this.button_clear.Text = "Clear Butons";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::WysHost.Properties.Resources.WYS;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(361, 60);
+            this.pictureBox1.TabIndex = 25;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::WysHost.Properties.Resources.Background;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(-5, 453);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(391, 52);
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 26;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 27;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(43, 339);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(263, 96);
+            this.richTextBox1.TabIndex = 29;
+            this.richTextBox1.Text = "please enter id ";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(348, 499);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_submit);
+            this.Controls.Add(this.panel_wysView);
+            this.Controls.Add(this.pictureBox2);
+            this.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "WYS Sample";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_wysView;
+        private System.Windows.Forms.Button button_submit;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ WysHost/WysHost/Properties/Gate.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using WysHost;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ClientDemo0
+{
+    class Gate
+    {
+        public static bool flag = false;
+        //"10.0.0.9" - example of ip
+        public static int sendAndReciveFromDal(string targetIP, byte[] content, byte[] recive)
+        {
+            TcpClient tcpclnt = new TcpClient();
+            Console.WriteLine("Connecting.....");
+
+            tcpclnt.Connect(targetIP, 8080);
+            // use the ipaddress as in the server program
+
+            Console.WriteLine("Connected");
+            Stream stm = tcpclnt.GetStream();
+
+            byte[] send = new byte[content.Length + 1];
+            send[0] = (byte)1;
+            for (int i = 0; i < content.Length; i++)
+            {
+                send[i + 1] = content[i];
+            }
+            stm.Write(send, 0, send.Length);
+            int size = stm.Read(recive, 0, 100);
+
+            tcpclnt.Close();
+            return size;
+
+        }
+        public static int sendAndReciveFromHost(string targetIP, byte[] content, byte[] recive)
+        {
+            TcpClient tcpclnt = new TcpClient();
+            Console.WriteLine("Connecting.....");
+
+            tcpclnt.Connect(targetIP, 8080);
+            // use the ipaddress as in the server program
+
+            Console.WriteLine("Connected");
+            Stream stm = tcpclnt.GetStream();
+
+            byte[] send = new byte[content.Length + 1];
+            send[0] = (byte)0;
+            for (int i = 0; i < content.Length; i++)
+            {
+                send[i + 1] = content[i];
+            }
+            stm.Write(send, 0, send.Length);
+            int size = stm.Read(recive, 0, 100);
+
+            tcpclnt.Close();
+            return size;
+
+        }
+        public static void checkAndDoFromServer(byte[] arr)
+        {
+            if (arr == null)
+                throw new Exception("NULL_FROM_SERVER");
+            switch ((int)arr[0])
+            {
+                case 0:
+                    //To Host
+                    break;
+                case 1:
+                    checkToDal(lessFirstByte((arr), arr.Length));
+                    //To Dal
+                    break;
+                default:
+                    //Do nothing
+                    break;
+            }
+        }
+
+        public static byte[] lessFirstByte(byte[] arr, int size)
+        {
+            --size;
+            byte[] result = new byte[size];
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = arr[i + 1];
+            }
+            return result;
+        }
+
+        public static void checkToDal(byte[] arr)
+        {
+            switch ((int)arr[0])
+            {
+                case 0:
+                    if ((int)arr[1] == 1)
+                    {
+                        flag = true;
+                        MessageBox.Show("please chose");
+                        sendToDal(10);
+                    }
+                    else if ((int)arr[1] == 0)
+                    {
+                        MessageBox.Show("id isnt corect");
+                        sendToDal(11);
+                    }
+                    break;
+                case 1:
+                    if ((int)arr[1] == 0)
+                    {
+                        MessageBox.Show("chosing error");
+                        sendToDal(12);
+                    }
+                    else if ((int)arr[1] == 1)
+                    {
+                        MessageBox.Show("you chose good job");
+                        sendToDal(13);
+                        flag = false;
+                    }
+                    break;
+                case 2:
+                    MessageBox.Show("error");
+                    sendToDal(14);
+                    break;
+                case 6:
+                    MessageBox.Show("error");
+                    sendToDal(14);
+                    break;
+                default:
+                    MessageBox.Show("error");
+                    sendToDal(14);
+                    break;
+            }
+        }
+        public static void sendToDal(int cmd)
+        {
+            const int otpLength = 10;
+            byte[] otpBytes = new byte[otpLength];
+            IntPtr outArr = Marshal.AllocHGlobal(otpLength);
+            WysWrapper.Sendrequestfordal(outArr, otpLength, cmd);
+        }
+    }
+}
